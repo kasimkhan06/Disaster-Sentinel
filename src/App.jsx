49628dcceafbd1f5b9287ba3c8dbe6798cb28f87
sparsh-kamat <kamatsparsh@gmail.com>
2025-04-03 11:37:42 +0530

    Revert "Merge branch 'main' of https://github.com/kasimkhan06/Disaster-Sentinel into pranavi"
    This reverts commit 2331176771302eaa4f5461d4db3946fe9b7ec731, reversing
    changes made to 119a0e2c2eeebc3540da1964ccb42d5f147205ca.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
 // import { Component } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CurrentLocation from './pages/disasters/user/CurrentLocation'
 import Home from './pages/dashboard/user/Home'
 import DisasterInfo from './pages/disasters/user/DisasterInfo'
-import Header from './components/HeaderAgency';
+import { Routes, Route } from "react-router-dom";
+import Header from './components/Header';
 import Login from './pages/authentication/login';
 import Register from './pages/authentication/register';
 import Verification from './pages/authentication/verification';
-import NGODashboard from './pages/agency/Agency';
-import RegistrationForm from './pages/dashboard/agency/RegistrationPage/RegistrationForm';
-import AgencyDashboard from './pages/dashboard/agency/agencyDashboard';
-import MissingPerson from './pages/dashboard/agency/Missing Person/MissingPerson';
-import PersonInfo from './pages/dashboard/agency/Missing Person/PersonInfo';
-import AnnouncementPage from "./pages/dashboard/agency/Announcement/Announcement";
-import FloodPrediction from "./pages/disasters/user/FloodPrediction";
-import Agencies from "./pages/disasters/user/Agencies";
 import DosDontsPage from './pages/training/dosdonts';
 import MissingPersonPortal from './pages/missingperson/portal';
 import StatusTracking from './pages/missingperson/status';
 
 
 const App = () => {
-  // let component
-  // switch (window.location.pathname) {
-
-  //     case '/home':
-  //         component = <Home />
-  //         break
-  //     case '/current-location':
-  //         component = <CurrentLocation />
-  //         break
-  //     case '/disaster-details':
-  //         component = <DisasterInfo />
-  //         break
-  // }
+    // let component
+    // switch (window.location.pathname) {
+        
+    //     case '/home':
+    //         component = <Home />
+    //         break
+    //     case '/current-location':
+    //         component = <CurrentLocation />
+    //         break
+    //     case '/disaster-details':
+    //         component = <DisasterInfo />
+    //         break
+    // }
   return (
     <>
     <Header /> Header is now wrapped within Router
@@ -45,22 +37,12 @@ const App = () => {
       <Route path="/verification" element={<Verification />} />
       <Route path="/current-location" element={<CurrentLocation />} />
       <Route path="/disaster-details/:id" element={<DisasterInfo />} />
-      <Route path="/flood-prediction" element={<FloodPrediction />} />
-      <Route path="/agencies" element={<Agencies />} />
-      <Route path="/agency" element={<NGODashboard />} />
-      <Route path="/registration-form" element={<RegistrationForm />} />
-      <Route path="/agency-dashboard" element={<AgencyDashboard />} />
-      <Route path="/missing-person" element={<MissingPerson />} />
-      <Route path="/person-details/:id" element={<PersonInfo />} />
-      <Route path="/announcement" element={<AnnouncementPage />} />
-      <Route path="/disaster-details/:id" element={<DisasterInfo />} />
       <Route path="/dosdontspage" element={<DosDontsPage />} />
       <Route path="/MissingPersonPortal" element={<MissingPersonPortal />} />
       <Route path="/status-tracking" element={<StatusTracking />} />
     </Routes>
     </>
-  );
-};
+  )
+}
 
-export default App;
 export default App;
