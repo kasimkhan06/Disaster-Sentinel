5ce8e7d76cc338d1fd159c88899ac7821063f0ea
Sachi Vinay Kamat <sachikamat2003@gmail.com>
2025-05-19 12:33:30 +0530

    reload after logging out from agency

--- src/components/HeaderAgency.jsx
+++ src/components/HeaderAgency.jsx
@@ -106,6 +106,7 @@ const Header = ({ isLoggedIn, setIsLoggedIn }) => {
         setIsLoggedIn(false);
         handleProfileMenuClose();
         navigate("/home");
+         window.location.reload();
       } else {
         console.error("Logout failed:", data.message || "Unknown error");
       }
